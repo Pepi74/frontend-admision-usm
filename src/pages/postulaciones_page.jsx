e6a8b3c34a6db8fd3738ad012524e0097c109c7c
Pepi74 <pepigaming74@gmail.com>
2024-11-04 19:21:14 -0300

    implementado aviso en postulaciones

--- src/pages/postulaciones_page.jsx
+++ src/pages/postulaciones_page.jsx
@@ -1,15 +1,39 @@
-import React from 'react'
+import React, {useState} from 'react'
 import Postulaciones from '../components/postulaciones'
 import Postular from '../components/postular'
 
 export const PostulacionesPage = () => {
+
+  const [showAlert, setShowAlert] = useState(true);
+
+  const handleCloseAlert = () => {
+    setShowAlert(false);
+  };
+
   return (
     <>
+    {showAlert && (
     <div style={{display: 'flex', justifyContent: 'center'}}> 
-        <div style={{backgroundColor: 'green', width: '50%', textAlign: 'center'}}>
-            Alerta de que si postulo por DEMRE.cl no es necesario postular por acá
+        <div style={{backgroundColor: '#82C364', height:'150px', width: '50%', textAlign: 'left', borderRadius: '10px', position: 'relative'}}>
+        <button 
+              onClick={handleCloseAlert} 
+              style={{
+                position: 'absolute',
+                top: '10px',
+                right: '10px',
+                background: 'transparent',
+                border: 'none',
+                fontSize: '20px',
+                cursor: 'pointer'
+              }}
+            >
+              X
+            </button>
+          <h2 style={{marginLeft:'20px'}}>RECUERDA</h2>
+          <p style={{marginLeft:'20px'}}>Si ya postulaste mediante <a href='https://www.demre.cl'>DEMRE.cl</a> no es necesario postular por este sitio.</p>
         </div>
     </div>
+    )}
     <br/>
     <br/>
     <div style={{display: 'flex',flexDirection: 'row', gap: '20px', justifyContent: 'center'}}>
